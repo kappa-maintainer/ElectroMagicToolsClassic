2ed5ea6d2d4ead745e96c6c90dc59a51a20b9e48
weissmoon <Weissmoon1@gmail.com>
2019-09-22 20:12:03 +0900

    Add Thaumium Drill

--- src/main/java/weissmoon/electromagictools/item/ModItems.java
+++ src/main/java/weissmoon/electromagictools/item/ModItems.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 import weissmoon.core.item.WeissItem;
 import weissmoon.core.item.armour.ItemArmourBase;
+import weissmoon.core.item.tools.WeissItemsPickaxe;
 import weissmoon.electromagictools.item.armour.boots.ItemElectricBootsTraveller;
 import weissmoon.electromagictools.item.armour.boots.ItemNanoBootsTraveller;
 import weissmoon.electromagictools.item.armour.boots.ItemQuantumBootsTraveller;
@@ -16,6 +17,7 @@ import weissmoon.electromagictools.item.armour.googles.ItemNanoGoggles;
 import weissmoon.electromagictools.item.armour.googles.ItemQuantumGoggles;
 import weissmoon.electromagictools.item.armour.googles.ItemSolarHelmetRevealing;
 import weissmoon.electromagictools.item.tool.ItemElectricHoeGrowth;
+import weissmoon.electromagictools.item.tool.ItemThaumiumDrill;
 
 /**
  * Created by Weissmoon on 9/3/19.
@@ -33,11 +35,11 @@ public class ModItems {
     public static final ItemHoe electricHoe = new ItemElectricHoeGrowth();
     public static final WeissItem materials = new ItemMaterials();
     public static final WeissItem onering = new ItemOneRing();
+    public static final WeissItemsPickaxe thaumiumDrill = new ItemThaumiumDrill();
 
 
     public static Item crafing;
     public static Item focusCristmas;
-    public static Item thaumiumDrill;
     public static Item diamondChainsaw;
     public static Item ironOmnitool;
     public static Item diamondOmnitool;
@@ -79,6 +81,7 @@ public class ModItems {
         GameRegistry.register(nanoBoots);
         GameRegistry.register(quantumBoots);
         GameRegistry.register(electricHoe);
+        GameRegistry.register(thaumiumDrill);
         GameRegistry.register(materials);
         GameRegistry.register(onering);
     }
--- src/main/java/weissmoon/electromagictools/item/tool/ItemThaumiumDrill.java
+++ src/main/java/weissmoon/electromagictools/item/tool/ItemThaumiumDrill.java
@@ -1,7 +1,124 @@
 package weissmoon.electromagictools.item.tool;
 
+import com.google.common.collect.Multimap;
+import ic2.api.item.ElectricItem;
+import ic2.api.item.IElectricItem;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import thaumcraft.api.ThaumcraftMaterials;
+import weissmoon.core.item.tools.WeissItemsPickaxe;
+import weissmoon.electromagictools.ElectroMagicTools;
+import weissmoon.electromagictools.lib.Strings;
+
 /**
  * Created by Weissmoon on 9/6/19.
  */
-public class ItemThaumiumDrill {
+public class ItemThaumiumDrill extends WeissItemsPickaxe implements IElectricItem{
+
+    protected int maxCharge, cost, tier = 0;
+    protected double transferLimit = 0;
+
+    public ItemThaumiumDrill() {
+        super(ThaumcraftMaterials.TOOLMAT_THAUMIUM, Strings.Items.THAUMIUM_DRILL_NAME);
+        this.maxCharge = 100000;
+        this.cost = 250;
+        this.tier = 2;
+        this.transferLimit = 100;
+        this.efficiency = 20F;
+        this.setCreativeTab(ElectroMagicTools.EMTtab);
+    }
+
+    public ItemThaumiumDrill(ToolMaterial material, String name) {
+        super(material, name);
+        this.maxCharge = 0;
+        this.cost = 0;
+        this.tier = 0;
+        this.transferLimit = 0;
+        this.efficiency = 0;
+        this.setCreativeTab(ElectroMagicTools.EMTtab);
+    }
+
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World world, IBlockState block, BlockPos pos, EntityLivingBase entityLiving) {
+        ElectricItem.manager.use(stack, cost, entityLiving);
+        return true;
+    }
+
+
+    @Override
+    public boolean canHarvestBlock(IBlockState block, ItemStack stack) {
+        return Items.DIAMOND_PICKAXE.canHarvestBlock(block, stack) ||
+                Items.DIAMOND_SHOVEL.canHarvestBlock(block, stack);
+    }
+
+    @Override
+    public float getDestroySpeed(ItemStack stack, IBlockState block) {
+        if (!ElectricItem.manager.canUse(stack, cost)) {
+            return 1.0F;
+        }
+        return super.getDestroySpeed(stack, block);
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack itemstack, EntityLivingBase entityliving, EntityLivingBase player){
+        ElectricItem.manager.use(itemstack, ((ItemThaumiumDrill)itemstack.getItem()).cost, player);
+        return true;
+    }
+
+    @Override
+    public boolean isRepairable()
+    {
+        return false;
+    }
+
+    @Override
+    public int getItemEnchantability() {
+        return 8;
+    }
+
+    @Override
+    public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack){
+        Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(slot);
+
+        if (slot == EntityEquipmentSlot.MAINHAND)
+        {
+            double attackDamage = 1.5;
+            if(ElectricItem.manager.canUse(stack, ((ItemThaumiumDrill)stack.getItem()).cost)) {
+                attackDamage = 5.5;
+            }
+            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", attackDamage, 0));
+            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", (double)this.attackSpeed, 0));
+        }
+
+        return multimap;
+        //return this.getItemAttributeModifiers(slot);
+    }
+
+    @Override
+    public boolean canProvideEnergy(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public double getMaxCharge(ItemStack stack) {
+        return this.maxCharge;
+    }
+
+    @Override
+    public int getTier(ItemStack stack) {
+        return this.tier;
+    }
+
+    @Override
+    public double getTransferLimit(ItemStack stack) {
+        return this.transferLimit;
+    }
 }
