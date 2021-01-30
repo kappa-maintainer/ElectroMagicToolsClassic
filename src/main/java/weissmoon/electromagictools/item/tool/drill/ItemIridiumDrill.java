4a02dd85206812ff99f7851a9bc1dbc43b9ec162
weissmoon <Weissmoon1@gmail.com>
2021-01-30 21:25:20 +0900

    Fix server crash, closes #3

--- src/main/java/weissmoon/electromagictools/item/tool/drill/ItemIridiumDrill.java
+++ src/main/java/weissmoon/electromagictools/item/tool/drill/ItemIridiumDrill.java
@@ -6,12 +6,8 @@ import net.minecraft.init.Enchantments;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import weissmoon.core.utils.NBTHelper;
 import weissmoon.electromagictools.lib.Strings;
 
@@ -32,32 +28,7 @@ public class ItemIridiumDrill extends ItemThaumiumDrill{
         tier = 2;
         transferLimit = 1000;
         efficiency = 25F;
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void onBlockBreak(BlockEvent.BreakEvent event){
-        if(event.getPlayer().getHeldItem(EnumHand.MAIN_HAND).getItem() instanceof ItemIridiumDrill){
-            boolean mode = NBTHelper.getBoolean(event.getPlayer().getHeldItem(EnumHand.MAIN_HAND), MODE_NBT_KEY);
-            if(mode){
-                event.setExpToDrop(event.getState().getBlock().getExpDrop(event.getState(), event.getWorld(), event.getPos(), 3));
-            }else if(event.getState().getBlock().canSilkHarvest(event.getWorld(), event.getPos(), event.getState(), event.getPlayer())){
-                event.setExpToDrop(0);
-            }
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void blockDrops(BlockEvent.HarvestDropsEvent event){
-        if(event.getHarvester() != null)
-            if(event.getHarvester().getHeldItem(EnumHand.MAIN_HAND).getItem() instanceof ItemIridiumDrill) {
-                boolean mode = NBTHelper.getBoolean(event.getHarvester().getHeldItem(EnumHand.MAIN_HAND), MODE_NBT_KEY);
-                if (mode) {
-                    List<ItemStack> drops = event.getState().getBlock().getDrops(event.getWorld(), event.getPos(), event.getState(), 3);
-                    event.getDrops().clear();
-                    event.getDrops().addAll(drops);
-                }
-            }
+        MinecraftForge.EVENT_BUS.register(new IridiumDrillEvent());
     }
 
     @Override
