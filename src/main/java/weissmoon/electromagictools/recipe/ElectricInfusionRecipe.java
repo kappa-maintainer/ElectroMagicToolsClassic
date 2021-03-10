88146b900870f8096d52b79d5b1ffdc4899d2da0
weissmoon <Weissmoon1@gmail.com>
2021-03-10 20:54:21 +0900

    Change ElectricInfusionRecipe upgraded output handling

--- src/main/java/weissmoon/electromagictools/recipe/ElectricInfusionRecipe.java
+++ src/main/java/weissmoon/electromagictools/recipe/ElectricInfusionRecipe.java
@@ -1,17 +1,15 @@
 package weissmoon.electromagictools.recipe;
 
+import ic2.api.classic.item.ICropAnalyzer;
+import ic2.api.classic.item.IEUReader;
+import ic2.api.classic.item.IThermometer;
 import ic2.api.item.ElectricItem;
 import ic2.api.item.IElectricItem;
-import ic2.core.item.armor.electric.ItemArmorNanoSuit;
-import ic2.core.item.armor.electric.ItemArmorQuantumSuit;
-import ic2.core.util.misc.StackUtil;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.crafting.InfusionRecipe;
-import weissmoon.electromagictools.item.ModItems;
+import weissmoon.core.utils.NBTHelper;
 import weissmoon.electromagictools.item.armour.googles.ItemNanoGoggles;
 
 import java.util.List;
@@ -27,7 +25,6 @@ public class ElectricInfusionRecipe extends InfusionRecipe{
     @Override
     public Object getRecipeOutput(EntityPlayer player, ItemStack input, List<ItemStack> comps ) {
         ItemStack itemOuput = ((ItemStack)this.recipeOutput).copy();
-        NBTTagCompound nbt = StackUtil.getOrCreateNbtData(itemOuput);
         if(itemOuput.getItem() instanceof IElectricItem) {
             double totalCharge = 0;
             if(input.getItem() instanceof IElectricItem){
@@ -41,50 +38,65 @@ public class ElectricInfusionRecipe extends InfusionRecipe{
             totalCharge = (totalCharge / 10) * 9;
             ElectricItem.manager.charge(itemOuput, totalCharge, 4, true, false);
         }
-        if (input.getItem() == ModItems.electricGoggles){
-            for (ItemStack eItem : comps) {
-                if (eItem.getItem() instanceof ItemArmorNanoSuit && ((ItemArmorNanoSuit)eItem.getItem()).armorType == EntityEquipmentSlot.HEAD){
-                    ItemArmorNanoSuit nanoSuit = (ItemArmorNanoSuit) eItem.getItem();
-                    if (nanoSuit.isEUReader(eItem)){
-                        nbt.setBoolean("EUReaderUpgrade", true);
-                    }
-                    if (nanoSuit.isCropAnalyzer(eItem)){
-                        nbt.setBoolean("CropUpgrade", true);
-                    }
-                    if (nanoSuit.isThermometer(eItem)){
-                        nbt.setBoolean("ThermometerUpgrade", true);
-                    }
-                    break;
+        if (itemOuput.getItem() instanceof ItemNanoGoggles){
+            if(input.getItem() instanceof IEUReader)
+                if (((IEUReader)input.getItem()).isEUReader(input)){
+                    NBTHelper.setBoolean(itemOuput, "EUReaderUpgrade", true);
                 }
+            if(input.getItem() instanceof ICropAnalyzer)
+                if (((ICropAnalyzer)input.getItem()).isCropAnalyzer(input)){
+                    NBTHelper.setBoolean(itemOuput, "CropUpgrade", true);
+                }
+            if(input.getItem() instanceof IThermometer)
+                if (((IThermometer)input.getItem()).isThermometer(input)){
+                    NBTHelper.setBoolean(itemOuput, "ThermometerUpgrade", true);
+                }
+            for (ItemStack eItem : comps) {
+//                if (eItem.getItem() instanceof ItemArmorNanoSuit && ((ItemArmorNanoSuit)eItem.getItem()).armorType == EntityEquipmentSlot.HEAD){
+//                    ItemArmorNanoSuit nanoSuit = (ItemArmorNanoSuit) eItem.getItem();
+                    if(eItem.getItem() instanceof IEUReader)
+                        if (((IEUReader)eItem.getItem()).isEUReader(eItem)){
+                            NBTHelper.setBoolean(itemOuput, "EUReaderUpgrade", true);
+                        }
+                    if(eItem.getItem() instanceof ICropAnalyzer)
+                        if (((ICropAnalyzer)eItem.getItem()).isCropAnalyzer(eItem)){
+                            NBTHelper.setBoolean(itemOuput, "CropUpgrade", true);
+                        }
+                    if(eItem.getItem() instanceof IThermometer)
+                        if (((IThermometer)eItem.getItem()).isThermometer(eItem)){
+                            NBTHelper.setBoolean(itemOuput, "ThermometerUpgrade", true);
+                        }
+//                    break;
+//                }
             }
         }
-        if (input.getItem() == ModItems.nanoGoggles){
-            for (ItemStack eItem : comps) {
-                if (eItem.getItem() instanceof ItemArmorQuantumSuit && ((ItemArmorQuantumSuit)eItem.getItem()).armorType == EntityEquipmentSlot.HEAD){
-                    ItemArmorQuantumSuit nanoSuit = (ItemArmorQuantumSuit) eItem.getItem();
-                    if (nanoSuit.isEUReader(eItem)){
-                        nbt.setBoolean("EUReaderUpgrade", true);
-                    }
-                    if (nanoSuit.isCropAnalyzer(eItem)){
-                        nbt.setBoolean("CropUpgrade", true);
-                    }
-                    if (nanoSuit.isThermometer(eItem)){
-                        nbt.setBoolean("ThermometerUpgrade", true);
-                    }
-                    break;
-                }
-            }
-            ItemNanoGoggles nanoSuit = (ItemNanoGoggles) input.getItem();
-            if (nanoSuit.isEUReader(input)){
-                nbt.setBoolean("EUReaderUpgrade", true);
-            }
-            if (nanoSuit.isCropAnalyzer(input)){
-                nbt.setBoolean("CropUpgrade", true);
-            }
-            if (nanoSuit.isThermometer(input)){
-                nbt.setBoolean("ThermometerUpgrade", true);
-            }
-        }
+//        if (input.getItem() == ModItems.nanoGoggles){
+//            for (ItemStack eItem : comps) {
+//                if (eItem.getItem() instanceof ItemArmorQuantumSuit && ((ItemArmorQuantumSuit)eItem.getItem()).armorType == EntityEquipmentSlot.HEAD){
+//                    ItemArmorQuantumSuit nanoSuit = (ItemArmorQuantumSuit) eItem.getItem();
+//                    if (nanoSuit.isEUReader(eItem)){
+//                        nbt.setBoolean("EUReaderUpgrade", true);
+//                    }
+//                    if (nanoSuit.isCropAnalyzer(eItem)){
+//                        nbt.setBoolean("CropUpgrade", true);
+//                    }
+//                    if (nanoSuit.isThermometer(eItem)){
+//                        nbt.setBoolean("ThermometerUpgrade", true);
+//                    }
+//                    break;
+//                }
+//            }
+//            ItemNanoGoggles nanoSuit = (ItemNanoGoggles) input.getItem();
+//            if (nanoSuit.isEUReader(input)){
+//                nbt.setBoolean("EUReaderUpgrade", true);
+//            }
+//            if (nanoSuit.isCropAnalyzer(input)){
+//                nbt.setBoolean("CropUpgrade", true);
+//            }
+//            if (nanoSuit.isThermometer(input)){
+//                nbt.setBoolean("ThermometerUpgrade", true);
+//            }
+//        }
         return itemOuput;
     }
 }
