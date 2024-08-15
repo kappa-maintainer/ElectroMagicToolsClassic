33597b02fa6d571dde00087f869e4b9223d1feea
arcade_kappa <gkappa@outlands.top>
2024-08-15 13:11:22 +0800

    Fix Pillar generation

--- src/main/java/weissmoon/electromagictools/world/ObsidianPillarGenerator.java
+++ src/main/java/weissmoon/electromagictools/world/ObsidianPillarGenerator.java
@@ -4,6 +4,8 @@ package weissmoon.electromagictools.world;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,20 +17,22 @@ import weissmoon.electromagictools.block.ModBlocks;
  * Created by Weissmoon on 2/18/21.
  */
 public class ObsidianPillarGenerator{
-//    WorldGenerator
     @SubscribeEvent
     public void generatePillar(DecorateBiomeEvent.Decorate event){
-//        Biome.
         if(event.getType() != DecorateBiomeEvent.Decorate.EventType.SAND_PASS2)
             return;
         World world = event.getWorld();
-        if (world.provider.getDimension() != 0)
+        if (world.provider.getDimensionType() != DimensionType.OVERWORLD)
             return;
 
-        int x = event.getChunkPos().getXStart() + RNGHelper.getRNGInt() % 8;
-        int z = event.getChunkPos().getZStart() + RNGHelper.getRNGInt() % 8;
+        ChunkPos cp = event.getChunkPos();
 
-        BlockPos pos = world.getTopSolidOrLiquidBlock(new BlockPos(x, 0, z));
+        int x = Math.abs(RNGHelper.getRNGInt() % 8);
+        int z = Math.abs(RNGHelper.getRNGInt() % 8);
+        int y = world.getChunk(cp.getXStart(), cp.getZStart()).getHeightValue(x, z);
+        x += cp.getXStart();
+        z += cp.getZStart();
+        BlockPos pos = new BlockPos(x, y, z);
         if(RNGHelper.getRNGFloat() < 0.999)
             return;
         if(!world.getBlockState(pos).isSideSolid(world, pos, EnumFacing.UP) || world.getBlockState(pos).getBlock() == ModBlocks.obsidianTile)
